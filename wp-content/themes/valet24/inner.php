cd1500d2404c1bcd5ac5c2a8fbe4cdf31ca11b2d
alextgco <alextgco@gmail.com>
2016-12-08 13:25:59 +0300

    asd

--- wp-content/themes/valet24/inner.php
+++ wp-content/themes/valet24/inner.php
@@ -110,77 +110,8 @@ include 'header.php';
 
         <div class="row">
 
-            <div class="col-md-3">
 
-<!--                <div class="sidebar-search-holder">-->
-<!--                    <i class="sidebar-search-icon fa fa-search"></i>-->
-<!--                    <input type="text" placeholder="Быстрый поиск" id="sidebar-search">-->
-<!--                </div>-->
-
-                <div class="sidebar-headline">Категории:</div>
-
-                <div class="category-sidebar">
-<!--                    -->
-
-
-                    <?php
-
-                        echo '<ul>';
-
-                        foreach($data as $node => $cat) {
-
-                            if ( strlen($cat[array_search('parent_category_id', $columns)]) > 0){
-
-                                echo '<li>' . $cat[array_search('parent_category_id', $columns)] . ' -- ' . $cat[array_search('name', $columns)] . '</li>';
-
-                            }else{
-
-                            }
-                        }
-
-                        echo '</ul>';
-                    ?>
-
-
-
-                    <ul id="sidebar-categories-menu" class="page-sidebar-menu">
-                        <li class="">
-                            <a href="" onclick="return false;">
-                                <div class="root-category-image-holder"><img src="<?php echo get_stylesheet_directory_uri() ?>/assets/img/cat-2.jpg"></div>
-                                <div class="menu-title">Алкоголь</div>
-                            </a>
-                            <ul class="sub-menu">
-                                <li class="menu-item"><a href="#" onclick="return false;">Вино</a></li>
-                                <li class="menu-item"><a href="#" onclick="return false;">Водка</a></li>
-                                <li class="menu-item"><a href="#" onclick="return false;">Пиво</a></li>
-                                <li class="menu-item"><a href="#" onclick="return false;">Коньяк</a></li>
-                            </ul>
-                        </li>
-                        <li class="">
-                            <a href="" onclick="return false;">
-                                <div class="root-category-image-holder"><img src="<?php echo get_stylesheet_directory_uri() ?>/assets/img/cat-3.jpg"></div>
-                                <div class="menu-title">Табак</div>
-                            </a>
-                            <ul class="sub-menu">
-                                <li class="">
-                                    <a href="" onclick="return false;">Сигареты<span class="arrow open"></span></a>
-                                    <ul class="sub-menu">
-                                        <li class="menu-item"><a href="#" onclick="return false;">Тонкие</a></li>
-                                        <li class="menu-item"><a href="#" onclick="return false;">Обычные</a></li>
-                                    </ul>
-                                </li>
-                                <li class="menu-item"><a href="#" onclick="return false;">Все для кальяна</a></li>
-                                <li class="menu-item"><a href="#" onclick="return false;">Сигары</a></li>
-                            </ul>
-                        </li>
-                    </ul>
-
-<!--                    -->
-                </div>
-
-            </div>
-
-            <div class="col-md-9">
+            <div class="col-md-12">
 
 
                 <?php
